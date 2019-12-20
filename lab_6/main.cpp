bf1c020809ca9a91c1dc8459b9c853b7a265daa6
aso <andreyw441@gmail.com>
2019-12-20 12:18:17 +0300

    lab 6 finished

---
+++ lab_6/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T, int size>
+class Vector{
+protected:
+    T vec[size];
+
+public:
+    Vector();
+    void print();
+    friend ostream& operator << (ostream& out, const Vector& vect){
+        for (int i = 0; i < size; ++i) {
+            out << vect.vec[i] << ' ';
+        }
+        return out;
+    }
+// TODO
+//    friend istream& operator >> (istream& in, const Vector& vect){
+//        cout << "Enter vector values:" << endl;
+//        for (int i = 0; i < size; ++i) {
+//            in >> vect.vec[i];
+//        }
+//        return in;
+//    }
+};
+
+template <class T, int size>
+Vector<T, size>::Vector() {
+    for (int i = 0; i < size; ++i) {
+        vec[i] = 0;
+    }
+}
+
+template <class T, int size>
+void Vector<T, size>::print() {
+    for (int i = 0; i < size; ++i) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int main(){
+    Vector<int, 5> v1;
+    v1.print();
+//    cin >> v1;
+    cout << v1;
+    return 0;
+}
